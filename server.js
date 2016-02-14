5f2d7ef06241de971ee0de7ee0bba6c9ca2d3d10
apple <apple@appledeMacBook-Pro.local>
2016-02-14 12:28:39 +0800

    final update

--- server.js
+++ server.js
@@ -16,9 +16,22 @@ app.get('/',function(req,res){
     res.send('Tdo API Root');
 });
 
-//GET  /TODOS
+//GET  /TODOS?completed=false&q=work
 app.get('/todos',function(req,res){
-   res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+    if(queryParams.hasOwnProperty('completed') && queryParams.completed ==='true'){
+        filteredTodos = _.where(filteredTodos,{completed:true})
+    }else if(queryParams.hasOwnProperty('completed') && queryParams.completed ==='false'){
+        filteredTodos = _.where(filteredTodos,{completed:false})
+    }
+
+    if(queryParams.hasOwnProperty('q') && queryParams.q.length>0){
+        filteredTodos = _.filter(filteredTodos,function(todo){
+            return todo.description.indexOf(queryParams.q.toLowerCase()) > -1;
+        })
+    }
+   res.json(filteredTodos);
 });
 
 app.get('/todos/:id',function(req,res){
@@ -47,6 +60,46 @@ app.post('/todos',function(req,res){
 
     res.json(body);
 });
+//DELETE REQ
+app.delete('/todos/:id',function(req,res){
+    var todoId = parseInt(req.params.id,10);
+    var matchedTodo = _.findWhere(todos,{id:todoId});
+
+    if(!matchedTodo){
+        res.status(404).json({'error':'no todo found wtih that id'});
+    }else{
+        todos = _.without(todos,matchedTodo);
+        res.json(todos);
+    }
+});
+//PUT /todos/:id
+app.put('/todos/:id',function(req,res){
+    var todoId = parseInt(req.params.id,10);
+    var matchedTodo = _.findWhere(todos,{id:todoId});
+
+    var body = _.pick( req.body,'description','completed') ;
+    var validAttributes = {};
+
+    if(!matchedTodo){
+        return res.status(404).send();
+    }
+
+    if(body.hasOwnProperty('completed') && _.isBoolean((body.completed))){
+        validAttributes.completed = body.completed;
+    }else if(body.hasOwnProperty('completed')){
+        return res.status(400).send();
+    }
+
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length>0){
+        validAttributes.description = body.description;
+    }else if(body.hasOwnProperty('description')){
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo,validAttributes);
+    res.json(matchedTodo);
+});
+
 
 app.listen(PORT,function(){
     console.log('express listening on port' + PORT +'!' );
