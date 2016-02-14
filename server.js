ecd0ebb59d91634fec24eb63092ed9298119f479
apple <apple@appledeMacBook-Pro.local>
2016-02-14 16:19:54 +0800

    db add

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var todos = [];
 var todoNextId = 1;
 var bodyParser = require('body-parser');
 var _ =require('underscore');
-
+var db = require('./db.js');
 
 app.use(bodyParser.json());
 
@@ -18,47 +18,83 @@ app.get('/',function(req,res){
 
 //GET  /TODOS?completed=false&q=work
 app.get('/todos',function(req,res){
-    var queryParams = req.query;
-    var filteredTodos = todos;
-    if(queryParams.hasOwnProperty('completed') && queryParams.completed ==='true'){
-        filteredTodos = _.where(filteredTodos,{completed:true})
-    }else if(queryParams.hasOwnProperty('completed') && queryParams.completed ==='false'){
-        filteredTodos = _.where(filteredTodos,{completed:false})
+    var query = req.query;
+    var where = {};
+
+    if(query.hasOwnProperty('completed') && query.completed === "true"){
+        where.completed =true;
+    }else if(query.hasOwnProperty('completed') && query.completed === 'false'){
+        where.completed = false;
     }
 
-    if(queryParams.hasOwnProperty('q') && queryParams.q.length>0){
-        filteredTodos = _.filter(filteredTodos,function(todo){
-            return todo.description.indexOf(queryParams.q.toLowerCase()) > -1;
-        })
+    if(query.hasOwnProperty('q') && query.q.length >0){
+        where.description = {
+            $like:'%'+query.q+'%'
+        };
     }
-   res.json(filteredTodos);
+
+    db.todo.findAll({where:where}).then(function(todos){
+        res.json(todos);
+    },function(e){
+        res.status(500).send(e);
+    });
+    //var queryParams = req.query;
+   // var filteredTodos = todos;
+   // if(queryParams.hasOwnProperty('completed') && queryParams.completed ==='true'){
+   //     filteredTodos = _.where(filteredTodos,{completed:true})
+   // }else if(queryParams.hasOwnProperty('completed') && queryParams.completed ==='false'){
+   //     filteredTodos = _.where(filteredTodos,{completed:false})
+   // }
+   //
+   // if(queryParams.hasOwnProperty('q') && queryParams.q.length>0){
+   //     filteredTodos = _.filter(filteredTodos,function(todo){
+   //         return todo.description.indexOf(queryParams.q.toLowerCase()) > -1;
+   //     })
+   // }
+   //res.json(filteredTodos);
 });
 
 app.get('/todos/:id',function(req,res){
     var todoId = parseInt(req.params.id,10);
-    var matchedTodo = _.findWhere(todos,{id:todoId});
-
-
-
-    if(matchedTodo){
-        res.json(matchedTodo);
-    }else{
-        res.status(404).send();
-    }
+    db.todo.findById(todoId).then(function(todo){
+        if(!!todo){
+            res.json(todo.toJSON());
+        }else{
+            res.status(400).send();
+        }
+    },function(e){
+        res.status(500).send(e);
+    });
+    //var matchedTodo = _.findWhere(todos,{id:todoId});
+    //
+    //
+    //
+    //if(matchedTodo){
+    //    res.json(matchedTodo);
+    //}else{
+    //    res.status(404).send();
+    //}
 });
 
 //POST
 app.post('/todos',function(req,res){
     var body = _.pick( req.body,'description','completed') ;
-    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
-        return res.status(400);
-    }
-    body.description = body.description.trim();
-    body.id = todoNextId++;
-    todos.push(body);
-    console.log('description: '+body.description);
 
-    res.json(body);
+    db.todo.create(body).then(function (todo) {
+        res.json(todo.toJSON());
+    }, function (e) {
+        res.status(400).json(e);
+    });
+
+    //if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
+    //    return res.status(400);
+    //}
+    //body.description = body.description.trim();
+    //body.id = todoNextId++;
+    //todos.push(body);
+    //console.log('description: '+body.description);
+    //
+    //res.json(body);
 });
 //DELETE REQ
 app.delete('/todos/:id',function(req,res){
@@ -100,7 +136,9 @@ app.put('/todos/:id',function(req,res){
     res.json(matchedTodo);
 });
 
-
-app.listen(PORT,function(){
-    console.log('express listening on port' + PORT +'!' );
+db.sequelize.sync().then(function(){
+    app.listen(PORT,function(){
+        console.log('express listening on port' + PORT +'!' );
+    });
 });
+
